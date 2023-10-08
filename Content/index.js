0437f9cb56760979b903c2003fe1646734b4da42
saivigneesh456 <85676204+saivigneesh456@users.noreply.github.com>
2023-10-08 14:59:21 +0000

    edit and deleted code

--- Content/index.js
+++ Content/index.js
@@ -1,40 +1,107 @@
-function addBlogPost() {
-    const title = document.getElementById('post-title').value;
-    const content = document.getElementById('post-content').value;
-    const image = document.getElementById('post-image').value;
-    const video = document.getElementById('post-video').value;
+document.addEventListener('DOMContentLoaded', function() {
+    const blogPostsContainer = document.getElementById('blog-posts');
+    const addButton = document.getElementById('add-post');
+    const updateButton = document.getElementById('update-post');
+    let editingPost = null;
 
-    const blogPost = document.createElement('div');
-    blogPost.className = 'blog-post';
+    addButton.addEventListener('click', function() {
+        addBlogPost();
+    });
 
-    const postTitle = document.createElement('h2');
-    postTitle.textContent = title;
+    updateButton.addEventListener('click', function() {
+        updateBlogPost();
+    });
 
-    const postContent = document.createElement('p');
-    postContent.textContent = content;
+    function addBlogPost() {
+        const title = document.getElementById('post-title').value;
+        const content = document.getElementById('post-content').value;
+        const image = document.getElementById('post-image').value;
+        const video = document.getElementById('post-video').value;
 
-    const postImage = document.createElement('img');
-    postImage.src = image;
-    postImage.alt = 'Blog Post Image';
+        const blogPost = document.createElement('div');
+        blogPost.className = 'blog-post';
 
-    const postVideo = document.createElement('iframe');
-    postVideo.src = video;
-    postVideo.width = '560';
-    postVideo.height = '315';
-    postVideo.frameborder = '0';
-    postVideo.allowfullscreen = true;
+        const postTitle = document.createElement('h2');
+        postTitle.textContent = title;
 
-    blogPost.appendChild(postTitle);
-    blogPost.appendChild(postContent);
-    blogPost.appendChild(postImage);
-    blogPost.appendChild(postVideo);
+        const postContent = document.createElement('p');
+        postContent.textContent = content;
 
-    const main = document.querySelector('main');
-    main.appendChild(blogPost);
+        const postImage = document.createElement('img');
+        postImage.src = image;
+        postImage.alt = 'Blog Post Image';
 
-    // Clear the form
-    document.getElementById('post-title').value = '';
-    document.getElementById('post-content').value = '';
-    document.getElementById('post-image').value = '';
-    document.getElementById('post-video').value = '';
-}
+        const postVideo = document.createElement('iframe');
+        postVideo.src = video;
+        postVideo.width = '560';
+        postVideo.height = '315';
+        postVideo.frameborder = '0';
+        postVideo.allowfullscreen = true;
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', function() {
+            editBlogPost(blogPost, title, content, image, video);
+        });
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', function() {
+            blogPost.remove();
+        });
+
+        blogPost.appendChild(postTitle);
+        blogPost.appendChild(postContent);
+        blogPost.appendChild(postImage);
+        blogPost.appendChild(postVideo);
+        blogPost.appendChild(editButton);
+        blogPost.appendChild(deleteButton);
+
+        blogPostsContainer.appendChild(blogPost);
+        clearForm();
+    }
+
+    function editBlogPost(post, title, content, image, video) {
+        editingPost = post;
+        document.getElementById('post-title').value = title;
+        document.getElementById('post-content').value = content;
+        document.getElementById('post-image').value = image;
+        document.getElementById('post-video').value = video;
+
+        addButton.style.display = 'none';
+        updateButton.style.display = 'block';
+    }
+
+    function updateBlogPost() {
+        if (editingPost) {
+            const title = document.getElementById('post-title').value;
+            const content = document.getElementById('post-content').value;
+            const image = document.getElementById('post-image').value;
+            const video = document.getElementById('post-video').value;
+
+            const postTitle = editingPost.querySelector('h2');
+            const postContent = editingPost.querySelector('p');
+            const postImage = editingPost.querySelector('img');
+            const postVideo = editingPost.querySelector('iframe');
+
+            postTitle.textContent = title;
+            postContent.textContent = content;
+            postImage.src = image;
+            postImage.alt = 'Blog Post Image';
+            postVideo.src = video;
+
+            clearForm();
+        }
+
+        addButton.style.display = 'block';
+        updateButton.style.display = 'none';
+        editingPost = null;
+    }
+
+    function clearForm() {
+        document.getElementById('post-title').value = '';
+        document.getElementById('post-content').value = '';
+        document.getElementById('post-image').value = '';
+        document.getElementById('post-video').value = '';
+    }
+});
